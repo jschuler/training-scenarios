448fed5e02669ec6dc40be8773c693f29eb2b478
Joachim Schuler <jschuler@redhat.com>
2020-07-30 13:13:25 -0400

    change path of tar to test my changes

--- react/react-basics/env-init.sh
+++ react/react-basics/env-init.sh
@@ -1,4 +1,4 @@
-curl -L https://raw.githubusercontent.com/patternfly/training-scenarios/master/react-site/pf-react-express.tar.gz > /root/pf-express.tar.gz
+curl -L https://raw.githubusercontent.com/jschuler/training-scenarios/master/react-site/pf-react-express.tar.gz > /root/pf-express.tar.gz
 tar -xvzf /root/pf-express.tar.gz
 npm install
 export CHOKIDAR_USEPOLLING=true
